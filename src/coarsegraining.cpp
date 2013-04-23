d081c1d484ed7605c0953cb820e3801fa74e72a5
ckloss <ckloss@fluid38.(none)>
2013-04-23 14:49:41 +0200

    added corasegraining

---
+++ src/coarsegraining.cpp
@@ -0,0 +1,78 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   LIGGGHTS is part of the CFDEMproject
+   www.liggghts.com | www.cfdem.com
+
+   Christoph Kloss, christoph.kloss@cfdem.com
+   Copyright 2009-2012 JKU Linz
+   Copyright 2012-     DCS Computing GmbH, Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   This software is distributed under the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+#include "lmptype.h"
+#include "mpi.h"
+#include "math.h"
+#include "stdlib.h"
+#include "string.h"
+#include "modify.h"
+#include "domain.h"
+#include "force.h"
+#include "neighbor.h"
+#include "error.h"
+#include "coarsegraining.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+Coarsegraining::Coarsegraining(LAMMPS *lmp) : Pointers(lmp)
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+Coarsegraining::~Coarsegraining()
+{
+}
+
+/* ----------------------------------------------------------------------
+   called as coarsegraining command in input script
+------------------------------------------------------------------------- */
+
+void Coarsegraining::command(int narg, char **arg)
+{
+  // error checks
+  // ensure is first in input script, so all commands using length scales
+  // can use it
+
+  //NP Update::Update sets skin to 0.3
+
+  if(neighbor->skin != 0.3 || domain->box_exist)
+    error->all(FLERR,"Illegal coarsegraining command, must use this command "
+                     "first in input script");
+
+  if(modify->nfix > 0)
+    error->all(FLERR,"Illegal coarsegraining command, must use this command "
+                     "before any 'fix' command");
+
+  // parse arguments
+
+  if (narg < 1) error->all(FLERR,"Illegal coarsegraining command");
+
+  int iarg = 0;
+  double cg = force->numeric(arg[iarg++]);
+  if(cg < 1.)
+    error->all(FLERR,"Illegal coarsegraining command, cg > 1 expected");
+
+  force->set_coarsegraining(cg);
+}
---
+++ src/coarsegraining.h
@@ -0,0 +1,50 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   LIGGGHTS is part of the CFDEMproject
+   www.liggghts.com | www.cfdem.com
+
+   Christoph Kloss, christoph.kloss@cfdem.com
+   Copyright 2009-2012 JKU Linz
+   Copyright 2012-     DCS Computing GmbH, Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   This software is distributed under the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+#ifdef COMMAND_CLASS
+
+CommandStyle(coarsegraining,Coarsegraining)
+
+#else
+
+#ifndef LMP_COARSEGRAINING_H
+#define LMP_COARSEGRAINING_H
+
+#include "stdio.h"
+#include "pointers.h"
+
+namespace LAMMPS_NS {
+
+class Coarsegraining : protected Pointers {
+ public:
+  Coarsegraining(class LAMMPS *);
+  ~Coarsegraining();
+  void command(int, char **);
+
+ private:
+
+};
+
+}
+
+#endif
+#endif
+
