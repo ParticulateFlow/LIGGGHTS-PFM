ed003e77df7332da39ac91b2a65b6ff9b2deb920
Andreas Aigner <andreas.aigner@jku.at>
2014-04-10 12:41:37 +0200

    Init for non-contact JKR force + Update of the example

--- src/normal_model_jkr.h
+++ src/normal_model_jkr.h
@@ -39,12 +39,13 @@ namespace ContactModels
   class NormalModel<JKR, Style> : protected Pointers
   {
   public:
-    static const int MASK = CM_REGISTER_SETTINGS | CM_CONNECT_TO_PROPERTIES | CM_COLLISION;
+    static const int MASK = CM_REGISTER_SETTINGS | CM_CONNECT_TO_PROPERTIES | CM_COLLISION | CM_NO_COLLISION;
 
     NormalModel(LAMMPS * lmp, IContactHistorySetup*) : Pointers(lmp),
       Yeff(NULL),
       Geff(NULL),
       betaeff(NULL),
+      cohEnergyDens(NULL),
       coefArea(NULL)
     {
       /*NL*/ printf("JKR loaded\n");
@@ -85,7 +86,7 @@ namespace ContactModels
       double Sn=2.*Yeff[itype][jtype]*sqrtval;
       double St=8.*Geff[itype][jtype]*sqrtval;
 
-      // original hertz model
+      // original Hertz model (required for other models)
       double kn=4./3.*Yeff[itype][jtype]*sqrtval;
       double kt=St;
       const double sqrtFiveOverSix = 0.91287092917527685576161630466800355658790782499663875;
@@ -97,27 +98,18 @@ namespace ContactModels
       kn /= force->nktv2p;
       kt /= force->nktv2p;
 
-      // area calculation (Newton faster than analytic solution) //NP modified A.A.
-      double area,areaOld,areaOld2,coef;
-      area = areaOld = reff;
-      for (int k=0; k<10; k++) {
-        areaOld = area;
-        areaOld2 = areaOld*areaOld;
-        coef = coefArea[itype][jtype]*reff*sqrt(areaOld);
-        area = (areaOld*areaOld2 + areaOld*coef + deltan*areaOld*reff)/(2*areaOld2 - coef); //NP modified sign of deltan
-        /*NL*/ //fprintf(screen,"deltan = %f, coefArea = %f, area = %f, areaOld = %f\n",deltan,coefArea[itype][jtype],area,areaOld);
-        if (fabs(area-areaOld) < __DBL_EPSILON__) break;
-      }
-      /*NL*/ if (isnan(area)) lmp->error->all(FLERR,"Area is NAN");
-      /*NL*/ //fprintf(screen,"coefArea = %f, area = %f\n",coefArea[itype][jtype],area);
-
+      // calculate contact area
+      const double area = calcContactArea(itype,jtype,reff,deltan);
 
       const double Fn_damping = -gamman*cdata.vn;
       const double area3 = area*area*area;
       const double Fn_contact = 4/3*Yeff[itype][jtype]*area3/reff - sqrt(16*M_PI*cohEnergyDens[itype][jtype]*Yeff[itype][jtype]*area3);
       const double Fn = Fn_contact + Fn_damping;
 
-      cdata.Fn = Fn;
+      //NP current implementation uses cohesive force for the calculation of the tangential force
+      //NP  this may be incorrect due to negative forces!
+      cdata.Fn = Fn; //NP save original normal Hertz force for tangential forces?
+      //NP save stiffness and damping coefficient of the original Hertz model for rolling resistance model, ...
       cdata.kn = kn;
       cdata.kt = kt;
       cdata.gamman = gamman;
@@ -140,13 +132,81 @@ namespace ContactModels
       }
     }
 
-    void noCollision(ContactData&, ForceData&, ForceData&){
-      // missing non contact force!!
+    void noCollision(ContactData& cdata, ForceData& i_forces, ForceData& j_forces){
+      //NP missing non contact force!!
+      //NP There is no way for non-contact, particle-wall forces!!
+      const int i = cdata.i;
+      const int j = cdata.j;
+      const int itype = atom->type[i];
+      const int jtype = atom->type[j];
+      const double ri = cdata.radi;
+      const double rj = cdata.radj;
+      const double reff=cdata.is_wall ? cdata.radi : (ri*rj/(ri+rj));
+      const double radsum = cdata.radsum;
+
+      // minimum deltan
+      const double minDeltaN = -1.5*cbrt(reff*cohEnergyDens[itype][jtype]*cohEnergyDens[itype][jtype]*M_PI*M_PI/(2*Yeff[itype][jtype]*Yeff[itype][jtype]));
+      const double r = sqrt(cdata.rsq);
+      const double deltan = radsum - r;
+      /*NL*/ //fprintf(screen,"Non-contact: minDeltaN = %f, deltan = %f\n",minDeltaN,deltan);
+
+      if (minDeltaN < deltan) { // non-contact force
+        const double area = calcContactArea(itype,jtype,reff,deltan);
+
+        //NP current state: no damping!
+        const double area3 = area*area*area;
+        const double Fn = 4/3*Yeff[itype][jtype]*area3/reff - sqrt(16*M_PI*cohEnergyDens[itype][jtype]*Yeff[itype][jtype]*area3);
+
+        double **x = atom->x;
+        const double rinv = 1.0 / r;
+        const double enx = (x[i][0] - x[j][0]) * rinv;
+        const double eny = (x[i][1] - x[j][1]) * rinv;
+        const double enz = (x[i][2] - x[j][2]) * rinv;
+
+
+        // apply normal force
+        if(cdata.is_wall) {
+          const double Fn_ = Fn * cdata.area_ratio;
+          i_forces.delta_F[0] = Fn_ * enx;
+          i_forces.delta_F[1] = Fn_ * eny;
+          i_forces.delta_F[2] = Fn_ * enz;
+        } else {
+          i_forces.delta_F[0] = Fn * enx;
+          i_forces.delta_F[1] = Fn * eny;
+          i_forces.delta_F[2] = Fn * enz;
+
+          j_forces.delta_F[0] = -i_forces.delta_F[0];
+          j_forces.delta_F[1] = -i_forces.delta_F[1];
+          j_forces.delta_F[2] = -i_forces.delta_F[2];
+        }
+
+        /*NL*/ //fprintf(screen,"Non-contact force: area = %f, Fn = %f\n",area,Fn);
+
+      } // no interaction
+
     }
     void beginPass(CollisionData&, ForceData&, ForceData&){}
     void endPass(CollisionData&, ForceData&, ForceData&){}
 
   protected:
+    inline double calcContactArea(const int itype, const int jtype, const double reff, const double deltan){
+      // area calculation (Newton faster than analytic solution)
+      //NP analytic solution of a quartic equation
+      double area = reff;
+      for (int k=0; k<10; k++) {
+        const double areaOld = area;
+        const double areaOld2 = areaOld*areaOld;
+        const double coef = coefArea[itype][jtype]*reff*sqrt(areaOld);
+        area = (areaOld*areaOld2 + areaOld*coef + deltan*areaOld*reff)/(2*areaOld2 - coef); //NP modified sign of deltan
+        /*NL*/ //fprintf(screen,"Loop: deltan = %f, coefArea = %f, area = %f, areaOld = %f\n",deltan,coefArea[itype][jtype],area,areaOld);
+        if (fabs(area-areaOld) < __DBL_EPSILON__) break;
+      }
+      /*NL*/ if (isnan(area)) lmp->error->all(FLERR,"Area is NAN. Cohesive surface energy to high?");
+      /*NL*/ //fprintf(screen,"Final: coefArea = %f, area = %f\n",coefArea[itype][jtype],area);
+
+      return area;
+    }
+
     double ** Yeff;
     double ** Geff;
     double ** betaeff;
