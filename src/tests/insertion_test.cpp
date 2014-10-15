3e1fdcc0ef31a3388834b4e3c65de6566cb7736e
Richard Berger <richard.berger@jku.at>
2014-10-15 13:45:55 +0200

    Added further unit-tests for discrete distribution

--- src/tests/insertion_test.cpp
+++ src/tests/insertion_test.cpp
@@ -6,10 +6,9 @@
 
 using namespace LAMMPS_NS;
 
-
 TEST(insertion, insertPack_1) {
-  char * argv[3] = {"liggghts", "-in", "scripts/in.insertPack"};
-  LAMMPS lammps(3, argv, MPI_COMM_WORLD);
+  const char * argv[3] = {"liggghts", "-in", "scripts/in.insertPack"};
+  LAMMPS lammps(3, const_cast<char**>(argv), MPI_COMM_WORLD);
   lammps.input->file();
 
   lammps.input->one("fix ins all insert/pack seed 100001 distributiontemplate pdd1 vel constant 0. 0. 0. insert_every once overlapcheck yes all_in yes particles_in_region 1 region reg");
@@ -19,8 +18,8 @@ TEST(insertion, insertPack_1) {
 }
 
 TEST(insertion, insertPack_150k) {
-  char * argv[3] = {"liggghts", "-in", "scripts/in.insertPack"};
-  LAMMPS lammps(3, argv, MPI_COMM_WORLD);
+  const char * argv[3] = {"liggghts", "-in", "scripts/in.insertPack"};
+  LAMMPS lammps(3, const_cast<char**>(argv), MPI_COMM_WORLD);
   lammps.input->file();
 
   lammps.input->one("fix ins all insert/pack seed 100001 distributiontemplate pdd1 vel constant 0. 0. 0. insert_every once overlapcheck yes all_in yes particles_in_region 150000 region reg");
