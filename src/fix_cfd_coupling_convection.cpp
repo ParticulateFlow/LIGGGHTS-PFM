3053c4fe17a862fa3169fce8237c73d15d3337cd
dqueteschiner <daniel.queteschiner@jku.at>
2015-08-10 12:47:41 +0200

    use fix specific error function

--- src/fix_cfd_coupling_convection.cpp
+++ src/fix_cfd_coupling_convection.cpp
@@ -128,12 +128,12 @@ void FixCfdCouplingConvection::init()
 {
     // make sure there is only one fix of this style
     if(modify->n_fixes_style(style) != 1)
-      error->all(FLERR,"More than one fix of style couple/cfd/dust/simple is not allowed");
+      error->fix_error(FLERR,this,"More than one fix of this style is not allowed");
 
     // find coupling fix
     fix_coupling = static_cast<FixCfdCoupling*>(modify->find_fix_style_strict("couple/cfd",0));
     if(!fix_coupling)
-      error->all(FLERR,"Fix couple/cfd/convection needs a fix of type couple/cfd");
+      error->fix_error(FLERR,this,"needs a fix of type couple/cfd");
 
     //values to send to OF
     fix_coupling->add_push_property("Temp","scalar-atom");
