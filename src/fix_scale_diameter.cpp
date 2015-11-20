480f0a6e016de5d53b00d196ddbde8334202c90d
dqueteschiner <daniel.queteschiner@jku.at>
2015-11-20 13:03:10 +0100

    add new fix class to scale particles within a spherical region
    Scale particles based on the distance to the center of a spherical
    region.

---
+++ src/fix_scale_diameter.cpp
@@ -0,0 +1,206 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   Copyright 2015-     JKU Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author:
+   Daniel Queteschiner (JKU Linz)
+------------------------------------------------------------------------- */
+
+#include "math.h"
+#include "string.h"
+#include "stdlib.h"
+#include "fix_scale_diameter.h"
+#include "atom.h"
+#include "update.h"
+#include "force.h"
+#include "domain.h"
+#include "error.h"
+#include "fix_property_atom.h"
+#include "region.h"
+
+using namespace LAMMPS_NS;
+using namespace FixConst;
+
+/* ---------------------------------------------------------------------- */
+
+FixScaleDiameter::FixScaleDiameter(LAMMPS *lmp, int narg, char **arg) :
+  Fix(lmp, narg, arg),
+  fix_property_(NULL),
+  scale_region(NULL),
+  idregion(NULL),
+  radius_(1.0),
+  scale_to_(0.0),
+  scale_range_(1.0)
+{
+  if (narg < 5) error->all(FLERR,"Not enough arguments for fix scale/diameter command");
+  nevery = force->inumeric(FLERR,arg[3]);
+  if (nevery < 0) error->all(FLERR,"Illegal fix scale/diameter command");
+
+  int iarg = 4;
+  bool hasargs = true;
+  while(iarg < narg && hasargs) {
+    if (strcmp(arg[iarg],"region") == 0) {
+        if (iarg+2 > narg) error->fix_error(FLERR,this,"Not enough arguments for 'region' option");
+      int iregion = domain->find_region(arg[iarg+1]);
+      if (iregion == -1) error->fix_error(FLERR,this,"Region ID does not exist");
+      int n = strlen(arg[iarg+1]) + 1;
+      idregion = new char[n];
+      strcpy(idregion,arg[iarg+1]);
+      scale_region = domain->regions[iregion];
+      if(strcmp(scale_region->style,"sphere") != 0)
+        error->fix_error(FLERR,this,"Region must be of style 'sphere'");
+      iarg += 2;
+      hasargs = true;
+    } else if (strcmp(arg[iarg],"scale") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix scale/diameter command");
+      scale_to_ = atof(arg[iarg+1]);
+      scale_range_ = 1. - scale_to_;
+      iarg += 2;
+      hasargs = true;
+    } else break;
+  }
+
+
+  if(!scale_region)
+    error->fix_error(FLERR,this,"No region defined");
+
+  radius_ = 0.5*(scale_region->extent_xhi - scale_region->extent_xlo);
+  center_[0] = scale_region->extent_xhi - radius_;
+  center_[1] = scale_region->extent_yhi - radius_;
+  center_[2] = scale_region->extent_zhi - radius_;
+
+  fix_property_ = NULL;
+}
+
+/* ---------------------------------------------------------------------- */
+
+FixScaleDiameter::~FixScaleDiameter()
+{
+  delete []idregion;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixScaleDiameter::post_create()
+{
+
+  if (fix_property_ == NULL)
+  {
+    char *fixid = new char[14+strlen(id)];
+    sprintf(fixid,"scale_d_orig_%s",id);
+
+    const char *fixarg[9];
+    // register property/atom
+    fixarg[0]=fixid;
+    fixarg[1]="all";
+    fixarg[2]="property/atom";
+    fixarg[3]=fixid;
+    fixarg[4]="scalar"; // 1 scalar per particle to be registered
+    fixarg[5]="yes";    // restart yes
+    fixarg[6]="no";     // communicate ghost no
+    fixarg[7]="no";     // communicate rev no
+    fixarg[8]="0.";     // take 0 as default radius
+    modify->add_fix(9,const_cast<char**>(fixarg));
+
+    fix_property_ = static_cast<FixPropertyAtom*>(modify->find_fix_property(fixid,"property/atom","scalar",0,0,style));
+    delete [] fixid;
+  }
+
+  double *radius = atom->radius;
+  int nlocal = atom->nlocal;
+
+  for (int i = 0; i < nlocal; ++i) {
+    fix_property_->vector_atom[i] = radius[i];
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixScaleDiameter::pre_delete(bool unfixflag)
+{
+    if (unfixflag && fix_property_) modify->delete_fix(fix_property_->id);
+}
+
+/* ---------------------------------------------------------------------- */
+
+int FixScaleDiameter::setmask()
+{
+  int mask = 0;
+  mask |= PRE_FORCE;
+  return mask;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixScaleDiameter::init()
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixScaleDiameter::setup_pre_force(int vflag)
+{
+  change_settings();
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixScaleDiameter::pre_force(int vflag)
+{
+  if (nevery == 0) return;
+  if (update->ntimestep % nevery) return;
+  change_settings();
+}
+
+
+/* ----------------------------------------------------------------------
+   change atom parameters based on distance to region center
+------------------------------------------------------------------------- */
+
+void FixScaleDiameter::change_settings()
+{
+  double **x = atom->x;
+  double *radius = atom->radius;
+  double *rmass = atom->rmass;
+  int *mask = atom->mask;
+  int nlocal = atom->nlocal;
+
+  for (int i = 0; i < nlocal; ++i) {
+    if (mask[i] & groupbit) {
+      if (!scale_region->match(x[i][0],x[i][1],x[i][2]))
+        continue;
+
+      const double delx = center_[0] - x[i][0];
+      const double dely = center_[1] - x[i][1];
+      const double delz = center_[2] - x[i][2];
+      const double rsq = delx * delx + dely * dely + delz * delz;
+
+      const double scale = scale_to_ + scale_range_*(sqrt(rsq)/radius_);
+      const double old_radius = radius[i];
+
+      if (fix_property_->vector_atom[i] <= 0.0) {
+        fix_property_->vector_atom[i] = old_radius;
+      }
+
+      radius[i] = scale * fix_property_->vector_atom[i];
+      const double relative_scale = radius[i]/old_radius;
+      rmass[i] *= relative_scale*relative_scale*relative_scale;
+    }
+  }
+}
---
+++ src/fix_scale_diameter.h
@@ -0,0 +1,66 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   Copyright 2015-     JKU Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing authors:
+   Daniel Queteschiner (JKU Linz)
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(scale/diameter,FixScaleDiameter)
+
+#else
+
+#ifndef LMP_FIX_SCALE_DIAMETER_H
+#define LMP_FIX_SCALE_DIAMETER_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixScaleDiameter : public Fix {
+ public:
+
+  FixScaleDiameter(class LAMMPS *, int, char **);
+  ~FixScaleDiameter();
+  int setmask();
+  void post_create();
+  void pre_delete(bool unfixflag);
+  void init();
+  void setup_pre_force(int);
+  void pre_force(int);
+
+ private:
+
+  void change_settings();
+
+  class FixPropertyAtom *fix_property_;
+  class Region *scale_region;
+  char *idregion;
+  double center_[3];
+  double radius_;
+  double scale_to_;
+  double scale_range_;
+};
+
+}
+
+#endif
+#endif
