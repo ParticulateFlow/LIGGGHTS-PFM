c4079bab18d82df461f1e5bce5605a228e97013b
Paul Kieckhefen <paul.kieckhefen@gmail.com>
2018-02-23 16:36:12 +0100

    initial for o2o comm. needs validation.

---
+++ src/cfd_datacoupling_one2one.h
@@ -0,0 +1,223 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   LIGGGHTS is part of the CFDEMproject
+   www.liggghts.com | www.cfdem.com
+
+   Christoph Kloss, christoph.kloss@cfdem.com
+   Copyright 2009-2012 JKU Linz
+   Copyright 2012-     DCS Computing GmbH, Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   This software is distributed under the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+#ifdef CFD_DATACOUPLING_CLASS
+
+   CfdDataCouplingStyle(one2one,CfdDatacouplingOne2One)
+
+#else
+
+#ifndef LMP_CFD_DATACOUPLING_ONE2ONE_H
+#define LMP_CFD_DATACOUPLING_ONE2ONE_H
+
+#include "cfd_datacoupling.h"
+#include "multisphere_parallel.h"
+#include "error.h"
+#include "properties.h"
+#include <mpi.h>
+#include <iostream>
+
+namespace LAMMPS_NS {
+
+class CfdDatacouplingOne2One : public CfdDatacoupling {
+ public:
+  CfdDatacouplingOne2One(class LAMMPS *, int,int, char **,class FixCfdCoupling*);
+  ~CfdDatacouplingOne2One();
+
+  void exchange();
+
+  virtual void pull(const char *name, const char *type, void *&ptr, const char *datatype);
+  virtual void push(const char *name, const char *type, void *&ptr, const char *datatype);
+
+  template <typename T> void pull_mpi(const char *,const char *,void *&,const int*,const int);
+  template <typename T> void push_mpi(const char *,const char *,void *&);
+
+  virtual bool error_push()
+  { return false;}
+
+ private:
+  template <typename T> MPI_Datatype mpi_type_dc();
+};
+
+/* ---------------------------------------------------------------------- */
+//NP OF to LIGGGHTS, global to local
+//NP ptr to is local, ptr from is global
+
+template <typename T>
+void CfdDatacouplingOne2One::pull_mpi
+(
+    const char *name,
+    const char *type,
+    void *&from,
+    const int* ids,
+    const int ncollected
+)
+{
+    int len1 = -1, len2 = -1;
+    // get reference where to write the data
+    void * to = find_pull_property(name,type,len1,len2);
+
+    if (atom->nlocal && (!to || len1 < 0 || len2 < 0))
+    {
+        if(screen) fprintf(screen,"LIGGGHTS could not find property %s to write data from calling program to.\n",name);
+        lmp->error->one(FLERR,"This is fatal");
+    }
+
+    // return if no data to transmit
+    if(len1*len2 < 1) return;
+
+    int m = 0;
+
+    T *from_t = (T*) from;
+    if(strcmp(type,"scalar-atom") == 0)
+    {
+        T *to_t = (T*) to;
+        for (int i = 0; i < ncollected; i++)
+        {
+            m = atom->map(ids[i]);
+            if (m >= 0)
+                to_t[m] = from_t[i];
+        }
+    }
+    else if(strcmp(type,"vector-atom") == 0)
+    {
+        T **to_t = (T**) to;
+        for (int i = 0; i < ncollected; i++)
+        {
+            m = atom->map(ids[i]);
+            if (m >= 0)
+            {
+                for (int j = 0; j < len2; j++)
+                {
+                    to_t[m][j] = from_t[i*len2+j];
+                }
+            }
+        }
+
+    }
+
+/*
+    // check memory allocation
+    T* allred = check_grow<T>(len1*len2);
+
+    // perform allreduce on incoming data - well, let's not
+    T **from_t = (T**)from;
+    MPI_Allreduce
+    (
+        &(from_t[0][0]),
+        &(allred[0]),
+        len1*len2,
+        mpi_type_dc<T>(),
+        MPI_SUM,
+        comm
+    );
+
+    // copy data - loops over max # global atoms, bodies
+    if(strcmp(type,"scalar-atom") == 0)
+    {
+        T *to_t = (T*) to;
+        for (int i = 0; i < len1; i++)
+            if ((m = atom->map(i+1)) >= 0)
+                to_t[m] = allred[i];
+    }
+    else if(strcmp(type,"vector-atom") == 0)
+    {
+        T **to_t = (T**) to;
+        for (int i = 0; i < len1; i++)
+            if ((m = atom->map(i+1)) >= 0)
+                for (int j = 0; j < len2; j++)
+                    to_t[m][j] = allred[i*len2 + j];
+    }
+    else if(strcmp(type,"scalar-multisphere") == 0)
+    {
+        T *to_t = (T*) to;
+        MultisphereParallel *ms_data = properties_->ms_data();
+        if(!ms_data)
+            error->one(FLERR,"Transferring a multisphere property from/to LIGGGHTS requires a fix multisphere");
+        for (int i = 0; i < len1; i++)
+            if ((m = ms_data->map(i+1)) >= 0)
+                to_t[m] = allred[i];
+    }
+    else if(strcmp(type,"vector-multisphere") == 0)
+    {
+        T **to_t = (T**) to;
+        MultisphereParallel *ms_data = properties_->ms_data();
+        if(!ms_data)
+            error->one(FLERR,"Transferring a multisphere property from/to LIGGGHTS requires a fix multisphere");
+        for (int i = 0; i < len1; i++)
+            if ((m = ms_data->map(i+1)) >= 0)
+                for (int j = 0; j < len2; j++)
+                    to_t[m][j] = allred[i*len2 + j];
+    }
+    else if(strcmp(type,"scalar-global") == 0 || strcmp(type,"vector-global") == 0 || strcmp(type,"matrix-global") == 0)
+    {
+        T **to_t = (T**) to;
+        for (int i = 0; i < len1; i++)
+            for (int j = 0; j < len2; j++)
+                to_t[i][j] = allred[i*len2 + j];
+    }
+*/
+    else error->one(FLERR,"Illegal data type in CfdDatacouplingOne2One::pull");
+}
+
+/* ---------------------------------------------------------------------- */
+//NP LIGGGHTS to OF, local to global
+//NP ptr from is local, ptr to is global
+//NP len1 is global # of datums (max tag)
+
+template <typename T>
+void CfdDatacouplingOne2One::push_mpi
+(
+    const char *name,
+    const char *type,
+    void *&to
+)
+{
+    error->one(FLERR,"Do not use one2one with twoWayMPI. ");
+}
+
+/* ---------------------------------------------------------------------- */
+
+template<typename T>
+inline MPI_Datatype CfdDatacouplingOne2One::mpi_type_dc()
+{
+    error->all(FLERR,"Illegal call to mpi_type_dc(), valid types are int and double");
+    return 0;
+}
+
+template<>
+inline MPI_Datatype CfdDatacouplingOne2One::mpi_type_dc<double>()
+{
+    return MPI_DOUBLE;
+}
+
+template<>
+inline MPI_Datatype CfdDatacouplingOne2One::mpi_type_dc<int>()
+{
+    return MPI_INT;
+}
+
+/* ---------------------------------------------------------------------- */
+
+}
+
+#endif
+#endif
---
+++ src/library_cfd_coupling_one2one.cpp
@@ -0,0 +1,225 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   LIGGGHTS is part of the CFDEMproject
+   www.liggghts.com | www.cfdem.com
+
+   Christoph Kloss, christoph.kloss@cfdem.com
+   Copyright 2009-2012 JKU Linz
+   Copyright 2012-     DCS Computing GmbH, Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   This software is distributed under the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   CFD-DEM Coupling Stuff
+------------------------------------------------------------------------- */
+
+#include <mpi.h>
+#include <string.h>
+#include "library_cfd_coupling_one2one.h"
+#include "lammps.h"
+#include "input.h"
+#include "atom.h"
+#include "update.h"
+#include "domain.h"
+#include "modify.h"
+#include "fix_cfd_coupling.h"
+#include "fix_multisphere.h"
+#include "cfd_regionmodel.h"
+#include "memory.h"
+#include "error.h"
+#include "comm.h"
+#include "cfd_datacoupling.h"
+#include "cfd_datacoupling_one2one.h"
+
+using namespace LAMMPS_NS;
+
+#define LMP_GROW_DELTA 11000
+/*NL*/ #define LMP_OF_DEBUG true
+
+double** o2o_liggghts_get_boundingbox(void *ptr)
+{
+    LAMMPS *lmp = (LAMMPS *) ptr;
+    double** bbox = new double*[2];
+    bbox[0] = lmp->domain->sublo;
+    bbox[1] = lmp->domain->subhi;
+    return bbox;
+}
+
+/* ---------------------------------------------------------------------- */
+
+int o2o_liggghts_get_maxtag(void *ptr)
+{
+  LAMMPS *lmp = (LAMMPS *) ptr;
+  return lmp->atom->tag_max();
+}
+
+/* ---------------------------------------------------------------------- */
+
+/*
+int o2o_liggghts_get_maxtag_ms(void *ptr)
+{
+  // currently no possibility to delete multisphere bodies
+  // so just return # of bodies
+
+  LAMMPS *lmp = (LAMMPS *) ptr;
+  FixMultisphere *fix_ms = static_cast<FixMultisphere*>(lmp->modify->find_fix_style_strict("multisphere",0));
+  if(!fix_ms) return 0;
+  return fix_ms->tag_max_body();
+}
+*/
+/* ---------------------------------------------------------------------- */
+/*
+int o2o_liggghts_get_ntypes_ms(void *ptr)
+{
+  // currently no possibility to delete multisphere bodies
+  // so just return # of bodies
+
+  LAMMPS *lmp = (LAMMPS *) ptr;
+  FixMultisphere *fix_ms = static_cast<FixMultisphere*>(lmp->modify->find_fix_style_strict("multisphere",0));
+  if(!fix_ms) return 0;
+  return fix_ms->ntypes();
+}
+*/
+/* ---------------------------------------------------------------------- */
+/*
+double* o2o_liggghts_get_vclump_ms(void *ptr)
+{
+  // currently no possibility to delete multisphere bodies
+  // so just return # of bodies
+
+  LAMMPS *lmp = (LAMMPS *) ptr;
+  FixMultisphere *fix_ms = static_cast<FixMultisphere*>(lmp->modify->find_fix_style_strict("multisphere",0));
+  if(!fix_ms) return 0;
+  return fix_ms->vclump();
+}
+*/
+/* ---------------------------------------------------------------------- */
+
+void* o2o_locate_coupling_fix(void *ptr)
+{
+    LAMMPS *lmp = (LAMMPS *) ptr;
+    int ifix = -1;
+    for(int i=0;i<lmp->modify->nfix;i++)
+      if(strcmp(lmp->modify->fix[i]->style,"couple/cfd") == 0)
+        ifix = i;
+
+    if(ifix ==-1) lmp->error->all(FLERR,"No fix of style 'couple/cfd' found, aborting.");
+
+    return ((void*)lmp->modify->fix[ifix]);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void o2o_data_liggghts_to_of
+(
+    const char *name,
+    const char *type,
+    void *ptr,
+    void *&data,
+    const char *datatype
+)
+{
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    CfdDatacouplingOne2One* dc = static_cast<CfdDatacouplingOne2One*>(fcfd->get_dc()); 
+
+    if(strcmp(datatype,"double") == 0)
+        dc->push_mpi<double>(name, type, data);
+    else if(strcmp(datatype,"int") == 0)
+        dc->push_mpi<int>(name, type, data);
+//    else error->one(FLERR,"Illegal call to CfdDatacouplingOne2One::pull_mpi, valid datatypes are 'int' and double'");
+}
+
+/* ---------------------------------------------------------------------- */
+
+void o2o_data_of_to_liggghts
+(
+    const char *name,
+    const char *type,
+    void *ptr,
+    void *data,
+    const char* datatype,
+    const int* ids,
+    const int ncollected
+)
+{
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    CfdDatacouplingOne2One* dc = static_cast<CfdDatacouplingOne2One*>(fcfd->get_dc());
+
+    if(strcmp(datatype,"double") == 0)
+        dc->pull_mpi<double>(name, type, data, ids, ncollected);
+    else if(strcmp(datatype,"int") == 0)
+        dc->pull_mpi<int>(name, type, data, ids, ncollected);
+ //   else error->one(FLERR,"Illegal call to CfdDatacouplingOne2One::pull, valid datatypes are 'int' and double'");
+
+}
+
+/* ---------------------------------------------------------------------- */
+
+//NP update region model
+void o2o_update_rm(void *ptr)
+{
+    LAMMPS *lmp = (LAMMPS *) ptr;
+    //FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+//    o2o_locate_coupling_fix(ptr);
+    //CfdRegionmodel *rm = fcfd->rm;
+
+    //NP call region model
+    //if(rm) rm->rm_update();
+    lmp->error->all(FLERR,"Region model update not implemented aborting.");
+}
+
+/* ---------------------------------------------------------------------- */
+
+void o2o_allocate_external_int(int    **&data, int len2,int len1,int    initvalue,void *ptr)
+{
+    //LAMMPS *lmp = (LAMMPS *) ptr;
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    fcfd->get_dc()->allocate_external(data,len2,len1,initvalue);
+}
+/* ---------------------------------------------------------------------- */
+
+void o2o_allocate_external_int(int    **&data, int len2,const char *keyword,int    initvalue,void *ptr)
+{
+    //LAMMPS *lmp = (LAMMPS *) ptr;
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    fcfd->get_dc()->allocate_external(data,len2,keyword,initvalue);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void o2o_allocate_external_double(double **&data, int len2,int len1,double initvalue,void *ptr)
+{
+    //LAMMPS *lmp = (LAMMPS *) ptr;
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    fcfd->get_dc()->allocate_external(data,len2,len1,initvalue);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void o2o_allocate_external_double(double **&data, int len2, int len1, const char* keyword,double initvalue,void *ptr)
+{
+    //LAMMPS *lmp = (LAMMPS *) ptr;
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    fcfd->get_dc()->allocate_external(data,len2,keyword,initvalue);
+}
+
+/* ---------------------------------------------------------------------- */
+
+//NP check if all requested quantities have been communicated
+//NP    since last call of this function
+void o2o_check_datatransfer(void *ptr)
+{
+    //LAMMPS *lmp = (LAMMPS *) ptr;
+    FixCfdCoupling* fcfd = (FixCfdCoupling*)o2o_locate_coupling_fix(ptr);
+    fcfd->get_dc()->check_datatransfer();
+}
