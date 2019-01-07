4de18889c7e4d1cb4d6c9c92e2c96c3dcfdde7fd
ekinaci <efekinac@yahoo.com>
2019-01-07 15:13:23 +0100

    include header

--- src/fix_cfd_coupling.cpp
+++ src/fix_cfd_coupling.cpp
@@ -35,6 +35,7 @@
 #include "fix_multisphere.h"
 #include "fix_property_global.h"
 #include "fix_property_atom.h"
+#include "fix_cfd_coupling.h"
 #include "style_cfd_datacoupling.h"
 #include "cfd_regionmodel.h"
 #include "style_cfd_regionmodel.h"
