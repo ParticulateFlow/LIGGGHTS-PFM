d91809737d513f1759853a9136b2d94c47487468
tlichtenegger <thomas.lichtenegger@jku.at>
2019-05-20 15:45:42 +0200

    Added group function idbound to return min/max ID of atoms in group.

--- src/group.h
+++ src/group.h
@@ -44,6 +44,8 @@ class Group : protected Pointers {
   double charge(int,int);
   void bounds(int, double *);              // bounds of atoms in group
   void bounds(int, double *, int);
+  void idbounds(int, int *);               // id bounds of atoms in group
+  void idbounds(int, int *, int);
   void xcm(int, double, double *);         // center-of-mass coords of group
   void xcm(int, double, double *, int);
   void vcm(int, double, double *);         // center-of-mass velocity of group
