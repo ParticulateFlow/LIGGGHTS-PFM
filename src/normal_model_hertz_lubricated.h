6071d76de6e5d2e9b7a458d47434b6a1038ffff1
tmjnijssen <tim@tim-nijssen.nl>
2022-03-08 18:32:56 +0100

    hertz/lubricated: employ noCollision()

--- src/normal_model_hertz_lubricated.h
+++ src/normal_model_hertz_lubricated.h
@@ -23,6 +23,7 @@
    Contributing authors:
    Christoph Kloss (JKU Linz, DCS Computing GmbH, Linz)
    Richard Berger (JKU Linz)
+   Tim M.J. Nijssen (TU Delft)
 ------------------------------------------------------------------------- */
 #ifdef NORMAL_MODEL
 NORMAL_MODEL(HERTZ_LUBRICATED,hertz/lubricated,9)
@@ -107,8 +108,6 @@ namespace ContactModels
       const double ri = cdata.radi;
       const double rj = cdata.radj;
       double reff = cdata.is_wall ? cdata.radi : (ri*rj/(ri+rj));
-      double * const deltav0 = &cdata.contact_history[history_offset];
-      double * const hmin  = &cdata.contact_history[history_offset+1];
 
       // gap height
       double hij;
@@ -116,44 +115,26 @@ namespace ContactModels
         hij = cdata.r - cdata.radi;
       else
         hij = cdata.r - cdata.radsum;
+      fprintf(screen,"h: %g\t",hij);  
       
       double Fn = 0.; // total normal force
-      double Fl = 0.; // lubrication force
-      double Fc = 0.; // contact force
 
       if (hij <= hco*reff) 
       {
-        // approach velocity
-        const double oldDeltav0 = *deltav0;
-        if (cdata.vn<0. || hij<*hmin)
-          *deltav0 = MAX(*deltav0,-cdata.vn);
-        else
-          *deltav0 = 0.;
-        
-        const double mul = coeffMu[itype][jtype]; //FIXME: couple with CFD
+        fprintf(screen,"vn: %g\t",cdata.vn);
 
-        // approach distance
-        if (*deltav0!=oldDeltav0 && *deltav0>0.) {
-
-          double YoungsModulusEff;
-          if (correctYoungsModulus)
-            YoungsModulusEff = YeffOriginal[itype][jtype];
-          else
-            YoungsModulusEff = Yeff[itype][jtype];
-
-          const double hmine = 0.37 * pow(mul**deltav0/YoungsModulusEff,0.4) * pow(reff,0.6);
-          *hmin = MAX(hminSigma[itype][jtype],hmine);
-        }
+        // minimum approach distance
+        const double hmin = compute_minimum_approach_distance(cdata, itype, jtype, cdata.vn, hij, reff);
 
         // lubrication force
-        if (hij>0.)
-          Fl = -6.*M_PI*mul*cdata.vn*reff*reff/MAX(hij,*hmin);
+        double Fl = compute_lubrication_force(itype, jtype, cdata.vn, reff, hij, hmin);
 
         // contact force
-        if (hij<=*hmin)
+        double Fc = 0.;
+        if (hij<=hmin)
         {
           // overlap
-          const double deltan = MAX(*hmin - hij, 0.);
+          const double deltan = MAX(hmin - hij, 0.);
           
           const double meff = cdata.meff;
           const double sqrtval = sqrt(reff*deltan);
@@ -182,56 +163,201 @@ namespace ContactModels
           Fc = Fc_damping + Fc_contact;
 
           //limit force to avoid the artefact of negative repulsion force
-          if(limitForce && (Fc<0.0) )
-          {
+          if (limitForce && (Fc<0.0))
             Fc = 0.;
-          }
+
+          fprintf(screen,"Fc: %g\t",Fc);
 
           cdata.kn = kn;
           cdata.kt = kt;
           cdata.gamman = gamman;
           cdata.gammat = gammat;
         }
-
-        // add to total force
-        if (hij>*hmin)
-          Fn = Fl;
-        else if (hij<=0.)
-          Fn = Fc;
-        else 
-        {
-          double frac = hij / *hmin;
-          Fn = frac*Fl + (1-frac)*Fc;
-        }
         cdata.Fn = Fc; // store only contact force for friction calculation in tangential model
-      }  
 
-      fprintf(screen,"h: %g\t",hij);
-      fprintf(screen,"vn: %g\t",cdata.vn);
-      fprintf(screen,"vn0: %g\t",*deltav0);
-      fprintf(screen,"hmin: %g\t",*hmin);
-      fprintf(screen,"Fl: %g\t",Fl); 
-      fprintf(screen,"Fc: %g\t",Fc);
-      fprintf(screen,"Fn: %g\t",Fn);
+        // total normal force
+        Fn = compute_total_normal_force(Fl, Fc, hij, hmin);
 
-      // apply total normal force
-      if(cdata.is_wall) {
-        const double Fn_ = Fn * cdata.area_ratio;
-        i_forces.delta_F[0] = Fn_ * cdata.en[0];
-        i_forces.delta_F[1] = Fn_ * cdata.en[1];
-        i_forces.delta_F[2] = Fn_ * cdata.en[2];
-      } else {
-        i_forces.delta_F[0] = Fn * cdata.en[0];
-        i_forces.delta_F[1] = Fn * cdata.en[1];
-        i_forces.delta_F[2] = Fn * cdata.en[2];
+        // apply total normal force
+        if(cdata.is_wall) {
+          const double Fn_ = Fn * cdata.area_ratio;
+          i_forces.delta_F[0] = Fn_ * cdata.en[0];
+          i_forces.delta_F[1] = Fn_ * cdata.en[1];
+          i_forces.delta_F[2] = Fn_ * cdata.en[2];
+        } else {
+          i_forces.delta_F[0] = Fn * cdata.en[0];
+          i_forces.delta_F[1] = Fn * cdata.en[1];
+          i_forces.delta_F[2] = Fn * cdata.en[2];
 
-        j_forces.delta_F[0] = -i_forces.delta_F[0];
-        j_forces.delta_F[1] = -i_forces.delta_F[1];
-        j_forces.delta_F[2] = -i_forces.delta_F[2];
+          j_forces.delta_F[0] = -i_forces.delta_F[0];
+          j_forces.delta_F[1] = -i_forces.delta_F[1];
+          j_forces.delta_F[2] = -i_forces.delta_F[2];
+        }
       }
+      else 
+      {
+        // apply no force
+        for (int ii=0; ii<3; ii++)
+        {
+          i_forces.delta_F[ii] = 0.;
+          if(!cdata.is_wall)
+            j_forces.delta_F[ii] = 0.;
+        }
+      }
+      fprintf(screen,"\n");  
     }
 
-    void noCollision(ContactData&, ForceData&, ForceData&){}
+    void noCollision(ContactData& cdata, ForceData & i_forces, ForceData & j_forces)
+    {
+      // compute approach properties
+      const double ri = cdata.radi;
+      const double rj = cdata.radj;
+      const double reff = cdata.is_wall ? cdata.radi : (ri*rj/(ri+rj));
+      const double r = sqrt(cdata.rsq);
+
+      // gap height
+      double hij;
+      if(cdata.is_wall)
+        hij = r - cdata.radi;
+      else
+        hij = r - cdata.radsum;
+      fprintf(screen,"h: %g\t",hij);
+
+      double Fn = 0.;
+
+      if (hij <= hco*reff)
+      {
+        int *type = atom->type;
+        const int i = cdata.i;
+        const int j = cdata.j;
+        const int itype = type[i];
+        const int jtype = type[j];
+
+        // relative velocity
+        double **v = atom->v;
+        const double vr1 = v[i][0] - v[j][0];
+        const double vr2 = v[i][1] - v[j][1];
+        const double vr3 = v[i][2] - v[j][2];
+
+        // normal vector
+        double **x = atom->x;
+        const double rinv = 1./r;
+        const double enx = (x[i][0] - x[j][0])*rinv;
+        const double eny = (x[i][1] - x[j][1])*rinv;
+        const double enz = (x[i][2] - x[j][2])*rinv;
+
+        // normal velocity
+        const double vn = vr1 * enx + vr2 * eny + vr3 * enz;
+
+        // minimum approach distance
+        double hmin = compute_minimum_approach_distance(cdata, itype, jtype, vn, hij, reff);
+
+        // lubrication force
+        double Fl = compute_lubrication_force(itype, jtype, vn, reff, hij, hmin);
+
+        // contact force
+        // TODO contact force calculation
+        double Fc = 0.;
+        fprintf(screen,"Fc: %g\t",Fc);
+
+        // total normal force
+        Fn = compute_total_normal_force(Fl, Fc, hij, hmin);
+
+        // apply total normal force
+        if(cdata.is_wall) {
+          const double Fn_ = Fn * cdata.area_ratio;
+          i_forces.delta_F[0] = Fn_ * enx;
+          i_forces.delta_F[1] = Fn_ * eny;
+          i_forces.delta_F[2] = Fn_ * enz;
+        } else {
+          i_forces.delta_F[0] = Fn * enx;
+          i_forces.delta_F[1] = Fn * eny;
+          i_forces.delta_F[2] = Fn * enz;
+
+          j_forces.delta_F[0] = -i_forces.delta_F[0];
+          j_forces.delta_F[1] = -i_forces.delta_F[1];
+          j_forces.delta_F[2] = -i_forces.delta_F[2];
+        }
+      }
+      else
+      {
+        // apply no force
+        for (int ii=0; ii<3; ii++)
+        {
+          i_forces.delta_F[ii] = 0.;
+          if(!cdata.is_wall)
+            j_forces.delta_F[ii] = 0.;
+        }
+      }
+      cdata.has_force_update = true;
+      fprintf(screen,"\n");
+    }
+
+    double compute_minimum_approach_distance(ContactData & cdata, int itype, int jtype, double vn, double hij, double reff)
+    {
+      double * const deltav0 = &cdata.contact_history[history_offset];
+      double * const hmin  = &cdata.contact_history[history_offset+1];
+
+      // approach velocity
+      const double oldDeltav0 = *deltav0;
+      if (vn<0. || hij<*hmin)
+        *deltav0 = MAX(*deltav0,-vn);
+      else
+        *deltav0 = 0.;
+        
+      const double mul = coeffMu[itype][jtype]; //FIXME: couple with CFD
+
+      // approach distance
+      if (*deltav0!=oldDeltav0 && *deltav0>0.)
+      {
+        double YoungsModulusEff;
+        if (correctYoungsModulus)
+          YoungsModulusEff = YeffOriginal[itype][jtype];
+        else
+          YoungsModulusEff = Yeff[itype][jtype];
+
+        const double hmine = 0.37 * pow(mul**deltav0/YoungsModulusEff,0.4) * pow(reff,0.6);
+        *hmin = MAX(hminSigma[itype][jtype],hmine);
+      }
+
+      fprintf(screen,"vn: %g\t",vn);
+      fprintf(screen,"vn0: %g\t",*deltav0);
+      fprintf(screen,"hmin: %g\t",*hmin);
+
+      return *hmin;
+    }
+
+    double compute_lubrication_force(int itype, int jtype, double vn, double reff, double hij, double hmin)
+    {
+      double Fl;
+      if (hij>0.)
+        Fl = -6.*M_PI*coeffMu[itype][jtype]*vn*reff*reff/MAX(hij,hmin);
+      else
+        Fl = 0.;
+
+      fprintf(screen,"Fl: %g\t",Fl); 
+      
+      return Fl;
+    }
+
+    double compute_total_normal_force(double Fl, double Fc, double hij, double hmin)
+    {
+      double Fn;
+      if (hij>hmin)
+        Fn = Fl;
+      else if (hij<=0.)
+        Fn = Fc;
+      else 
+      {
+        double frac = hij / hmin;
+        Fn = frac*Fl + (1-frac)*Fc;
+      }
+
+      fprintf(screen,"Fn: %g\t",Fn);
+
+      return Fn;
+    }
+
     void beginPass(CollisionData&, ForceData&, ForceData&){}
     void endPass(CollisionData&, ForceData&, ForceData&){}
 
