8c73ae401a44ca904418f6732e46ad0d70380022
tlichtenegger <thomas.lichtenegger@jku.at>
2022-07-05 08:57:53 +0200

    Added fix/limit/property that allows to specify min and max values for any fix/property/atom.

---
+++ src/fix_limit_property_atom.cpp
@@ -0,0 +1,163 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   LIGGGHTS is part of the CFDEMproject
+   www.liggghts.com | www.cfdem.com
+
+   Christoph Kloss, christoph.kloss@cfdem.com
+   Copyright 2009-2012 JKU Linz
+   Copyright 2012-     DCS Computing GmbH, Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   This software is distributed under the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fix_limit_property_atom.h"
+#include "atom.h"
+#include "memory.h"
+#include "error.h"
+
+//NP modified C.K.
+#include "atom_vec.h"
+#include "force.h"
+#include "update.h"
+#include "comm.h"
+#include "modify.h"
+
+#include "mpi_liggghts.h"
+
+using namespace LAMMPS_NS;
+using namespace FixConst;
+
+/* ---------------------------------------------------------------------- */
+
+FixLimitPropertyAtom::FixLimitPropertyAtom(LAMMPS *lmp, int narg, char **arg, bool parse) :
+  Fix(lmp, narg, arg)
+{
+    /*NL*/ //if (screen) fprintf(screen,"HERE parse for id %s\n",id);
+    if(parse) parse_args(narg,arg);
+}
+
+void FixLimitPropertyAtom::parse_args(int narg, char **arg)
+{
+    int n = strlen(arg[3]) + 1;
+    targetfixname = new char[n];
+    strcpy(targetfixname,arg[3]);
+    fix_target_ = NULL;
+
+    if (strcmp(arg[4],"values_to_limit")==0)
+    {
+        nvalues = atoi(arg[5]);
+        maxvalues = new double[nvalues];
+        minvalues = new double[nvalues];
+    }
+    else error->all(FLERR,"Specify number of limiting values in fix limit/property/atom.");
+
+    if (strcmp(arg[6],"min") == 0)
+    {
+        for (int j = 0; j < nvalues; j++)
+        {
+            minvalues[j] = atof(arg[7+j]);
+        }
+    }
+    else error->all(FLERR,"Expected to find minimum limiting values");
+
+    if (strcmp(arg[7+nvalues],"max") == 0)
+    {
+        for (int j = 0; j < nvalues; j++)
+        {
+            maxvalues[j] = atof(arg[8+nvalues+j]);
+        }
+    }
+    else error->all(FLERR,"Expected to find maximum limiting values");    
+}
+
+/* ---------------------------------------------------------------------- */
+
+FixLimitPropertyAtom::~FixLimitPropertyAtom()
+{
+    delete[] targetfixname;
+    delete[] maxvalues;
+    delete[] minvalues;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixLimitPropertyAtom::init()
+{
+    if (nvalues == 1)
+    {
+        fix_target_ = static_cast<FixPropertyAtom*>(modify->find_fix_property(targetfixname, "property/atom", "scalar", 0, 0, "fix/limit/property/atom"));
+    }
+    else
+    {
+        fix_target_ = static_cast<FixPropertyAtom*>(modify->find_fix_property(targetfixname, "property/atom", "vector", 0, 0, "fix/limit/property/atom"));
+    }
+
+    if (fix_target_->num_defaultvalues() != nvalues)
+    {
+        char errmsg[400];
+        sprintf(errmsg,"Number of values to be limited by FixLimitPropertyAtom does not match that stored in %s",targetfixname);
+        error->all(FLERR,errmsg);    
+    }
+}
+
+/* ---------------------------------------------------------------------- */
+
+int FixLimitPropertyAtom::setmask()
+{
+  int mask = 0;
+  mask |= END_OF_STEP;
+  return mask;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixLimitPropertyAtom::end_of_step()
+{
+    int nlocal = atom->nlocal;
+
+    if (nvalues == 1)
+    {
+        double *targetvalue = fix_target_->vector_atom;
+        for (int i = 0; i < nlocal; ++i)
+        {
+            if (targetvalue[i] < minvalues[0])
+            {
+                targetvalue[i] = minvalues[0];
+            }
+            else if (targetvalue[i] > maxvalues[0])
+            {
+                targetvalue[i] = maxvalues[0];
+            }
+        }
+    }
+    else
+    {
+        double **targetvalue = fix_target_->array_atom;
+        for (int i = 0; i < nlocal; ++i)
+        {
+            for (int j = 0; j < nvalues; j++)
+            {
+                if (targetvalue[i][j] < minvalues[j])
+                {
+                    targetvalue[i][j] = minvalues[j];
+                }
+                else if (targetvalue[i][j] > maxvalues[j])
+                {
+                    targetvalue[i][j] = maxvalues[j];
+                }
+            }
+        }
+    }
+}
---
+++ src/fix_limit_property_atom.h
@@ -0,0 +1,59 @@
+/* ----------------------------------------------------------------------
+   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   Transfer Simulations
+
+   LIGGGHTS is part of the CFDEMproject
+   www.liggghts.com | www.cfdem.com
+
+   Christoph Kloss, christoph.kloss@cfdem.com
+   Copyright 2009-2012 JKU Linz
+   Copyright 2012-     DCS Computing GmbH, Linz
+
+   LIGGGHTS is based on LAMMPS
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   This software is distributed under the GNU General Public License.
+
+   See the README file in the top-level directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(limit/property/atom,FixLimitPropertyAtom)
+
+#else
+
+#ifndef LMP_FIX_LIMIT_PROPERTY_ATOM_H
+#define LMP_FIX_LIMIT_PROPERTY_ATOM_H
+
+#include "fix.h"
+#include "input.h"
+#include "fix_property_atom.h"
+
+namespace LAMMPS_NS {
+
+class FixLimitPropertyAtom : public Fix {
+ friend class Set;
+ public:
+  FixLimitPropertyAtom(class LAMMPS *, int, char **,bool parse = true);
+  ~FixLimitPropertyAtom();
+  void init();
+  virtual int setmask();
+  virtual void end_of_step();
+
+ protected:
+  virtual void parse_args(int narg, char **arg);
+
+  char *targetfixname;   // name of the fix property atom to be limited
+  class FixPropertyAtom* fix_target_;
+
+  int nvalues;
+  double *maxvalues;
+  double *minvalues;
+}; //end class
+
+}
+#endif
+#endif
